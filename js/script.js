9d1e8ad23a78c83250786570905dab7a2dcc5005
FiorellaQA <fiorella_90_17@hotmail.com>
2017-03-05 15:53:12 -0500

    primer commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var boton = document.getElementById("boton");
+var printResul = document.getElementById("printResul");
+document.getElementById("form1").addEventListener("submit",function(e){
+	e.preventDefault(); //Evita el envío del formulario hasta comprobar
+	//escribir el código aquí
+
+
+		
+});
